86e09a0c57983c72be69051da127621425241962
Lopez Astro <edwin12sv@gmail.com>
2022-08-28 22:48:17 -0600

    first commit

---
+++ TemperaturaRefuerzoUES/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace TemperaturaRefuerzoUES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtaInfo = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTemperatura = new System.Windows.Forms.TextBox();
+            this.txtHora = new System.Windows.Forms.TextBox();
+            this.txtMinuto = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtaInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(38, 286);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 0;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingresar una hora:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingresar los minutos:";
+            // 
+            // dtaInfo
+            // 
+            this.dtaInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtaInfo.Location = new System.Drawing.Point(38, 122);
+            this.dtaInfo.Name = "dtaInfo";
+            this.dtaInfo.Size = new System.Drawing.Size(480, 150);
+            this.dtaInfo.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(226, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ingrese temperatura en Celsius:";
+            // 
+            // txtTemperatura
+            // 
+            this.txtTemperatura.Location = new System.Drawing.Point(229, 40);
+            this.txtTemperatura.Name = "txtTemperatura";
+            this.txtTemperatura.Size = new System.Drawing.Size(152, 20);
+            this.txtTemperatura.TabIndex = 7;
+            // 
+            // txtHora
+            // 
+            this.txtHora.Location = new System.Drawing.Point(38, 43);
+            this.txtHora.Name = "txtHora";
+            this.txtHora.Size = new System.Drawing.Size(100, 20);
+            this.txtHora.TabIndex = 8;
+            this.txtHora.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtMinuto
+            // 
+            this.txtMinuto.Location = new System.Drawing.Point(38, 82);
+            this.txtMinuto.Name = "txtMinuto";
+            this.txtMinuto.Size = new System.Drawing.Size(100, 20);
+            this.txtMinuto.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 321);
+            this.Controls.Add(this.txtMinuto);
+            this.Controls.Add(this.txtHora);
+            this.Controls.Add(this.txtTemperatura);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtaInfo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEnviar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtaInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dtaInfo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTemperatura;
+        private System.Windows.Forms.TextBox txtHora;
+        private System.Windows.Forms.TextBox txtMinuto;
+    }
+}
+
---
+++ TemperaturaRefuerzoUES/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemperaturaRefuerzoUES
+{
+    public partial class Form1 : Form
+    {
+        public int contador = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            int hora = 0;
+            int minuto = 0;
+            double temperatura = 0;
+            string tiempo = String.Empty;
+            bool estadoHora = false;
+            bool estadoMinuto = false;
+            bool estadoTemperatura = false;
+            var dta = new DataTable();
+            try
+            {
+                hora = int.Parse(txtHora.Text);
+                minuto = Convert.ToInt32(txtMinuto.Text);
+                temperatura = Convert.ToDouble(txtTemperatura.Text);
+                if (hora >= 0 && hora <= 24)
+                {
+                    estadoHora = true;
+                }
+                else
+                {
+                    MessageBox.Show("Se ingreso una hora fuera de rango");
+                    txtHora.Focus();
+                }
+                if (minuto >= 0 && minuto < 60)
+                {
+                    estadoMinuto = true;
+                }
+                else 
+                {
+                    MessageBox.Show("Se ingresaron minutos fuera de rango");
+                    txtMinuto.Focus();
+                }
+                if (temperatura >= -270)
+                {
+                    estadoTemperatura = true;
+                }
+                else 
+                {
+                    MessageBox.Show("Se ingreso una temperatura fuera de rango");
+                    txtMinuto.Focus();
+                }
+                if (estadoHora && estadoMinuto && estadoTemperatura) 
+                {
+                    dta.Columns.Add("#");
+                    dta.Columns.Add("Temperatura (C°)");
+                    dta.Columns.Add("Tipo de Clima");
+                    dta.Columns.Add("Hora");
+                    if (temperatura < 10)
+                    {
+                        tiempo = hora + ":" + minuto;
+                        dta.Rows.Add(contador,temperatura, "Frio", tiempo);
+                        contador=contador+1;
+                        
+                    }
+                    else 
+                    {
+                        if (temperatura >= 10 && temperatura <= 20)
+                        {
+                            tiempo = hora + ":" + minuto;
+                            dta.Rows.Add(contador, temperatura, "Nublado", tiempo);
+                            contador = contador + 1;
+                        }
+                        else 
+                        {
+                            tiempo = hora + ":" + minuto;
+                            dta.Rows.Add(contador, temperatura, "Calor", tiempo);
+                            contador = contador + 1;
+                        }
+                    }
+                    dtaInfo.DataSource = dta;
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("Sucedio un error!");
+            }
+        }
+    }
+}
